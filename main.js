1fc3c43404ac17e330b6aeb51d0c17a8e1c06b20
Tinatin <tinatinatsikobadze@gmail.com>
2023-02-08 01:24:14 +0400

    Second page section

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const resumeButton = document.querySelector(".resumeButton");
 const mainPage = document.querySelector(".mainPage");
 const personalInfo = document.querySelector(".personalInfo");
-let backButton = document.querySelector(".personal_info_button");
+const backButton = document.querySelector(".personal_info_button");
+const nextPageButton = document.querySelector(".nextPageButton");
+const experienceSection = document.querySelector(".experienceSection");
+const pages = document.querySelector(".pages");
 resumeButton.addEventListener("click", function () {
   mainPage.style.display = "none";
   personalInfo.style.display = "block";
@@ -9,4 +12,10 @@ resumeButton.addEventListener("click", function () {
 backButton.addEventListener("click", function () {
   personalInfo.style.display = "none";
   mainPage.style.display = "block";
+});
+nextPageButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  personalInfo.style.display = "none";
+  pages.textContent = "2/3";
+  experienceSection.style.display = "block";
 });
